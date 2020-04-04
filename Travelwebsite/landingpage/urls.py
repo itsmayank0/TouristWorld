61af695702e53d0ab3291ca7cca61413dc418911
mayank0 <mayankirar@gmail.com>
2020-04-04 22:42:00 +0530

    data Updation in DB with Forms

--- Travelwebsite/landingpage/urls.py
+++ Travelwebsite/landingpage/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('addDestination/formsubmission', views.formsubmission, name="formsubmission"),
+    path('addDestination/', views.formsubmission, name="pade")
 ]
--- Travelwebsite/landingpage/views.py
+++ Travelwebsite/landingpage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import destination
 # Create your views here.
 def index(request):
@@ -8,3 +8,21 @@ def index(request):
     return render(request, 'index.html',{'dests': dests})
 
 # python manage.py runserver
+def formsubmission(request):
+    if request.method == 'GET':
+        return render(request, 'adddest.html')
+    else:
+        img = request.POST['img']
+        description = request.POST['description']
+        price = request.POST['price']
+        title = request.POST['title']
+        offer = request.POST['offer']
+        if offer=='no':
+            offer=0
+        else:
+            offer=1
+        reviews = request.POST['reviews']
+        #images not uploaded here
+        obj = destination(offer=offer,img=img, description=description,title=title,price=price,reviews=reviews)
+        obj.save()
+        return redirect('/')
