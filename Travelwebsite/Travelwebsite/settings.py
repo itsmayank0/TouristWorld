6222ebea22b1ba978bb8f20280309dc665fc0e51
itsmayank0 <mayankirar@gmail.com>
2020-05-18 10:27:51 +0530

    "Comments added for better understending"

--- Travelwebsite/Travelwebsite/settings.py
+++ Travelwebsite/Travelwebsite/settings.py
@@ -135,6 +135,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'sapnaravat0759@gmail.com'
-EMAIL_HOST_PASSWORD = 'mayankirar@123'
+EMAIL_HOST_USER = 'yourmail@yourmail.com'
+EMAIL_HOST_PASSWORD = 'YourPassword'
 ##before sending mail through gmail make sure to turn on less secure app setting in gmail
