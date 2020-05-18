c59117edf87e291a96b5653ce99874a25ccd479b
itsmayank0 <mayankirar@gmail.com>
2020-05-18 10:18:18 +0530

    Type Bug Fix

--- Travelwebsite/destinations/views.py
+++ Travelwebsite/destinations/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import destinations
 # Create your views here.
+
+
 def destination_page(request):
     if not request.user.is_authenticated:
         messages.info(request,"Please Login to access Destionations!")
